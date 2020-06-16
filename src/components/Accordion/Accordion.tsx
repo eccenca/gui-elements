bec190f182ffb1cf8401442d4a9de2ca24cb519c
Michael Haschke <mhaschke@brox.de>
2020-06-16 14:53:48 +0200

    add interface description to Accordion elements

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { Accordion as CarbonAccordion } from "carbon-components-react";
 
-function Accordion({ children, className = "", align = "start", ...otherProps }: any) {
+interface IAccordionProps extends React.HTMLAttributes<HTMLUListElement> {
+    /**
+        space-delimited list of class names
+    */
+    className?: string;
+    /**
+        place of toggler, "start" (default) or "end"
+    */
+    align?: "start" | "end";
+}
+
+function Accordion({ children, className = "", align = "start", ...otherProps }: IAccordionProps) {
     return (
         <CarbonAccordion className={"ecc-accordion__container " + className} align={align} {...otherProps}>
             {children}
--- src/components/Accordion/AccordionItem.tsx
+++ src/components/Accordion/AccordionItem.tsx
@@ -1,6 +1,40 @@
 import React from "react";
 import { AccordionItem as CarbonAccordionItem } from "carbon-components-react";
 
+interface IAccordionItemProps {
+    children?: JSX.Element | false | null | undefined;
+    /**
+        space-delimited list of class names
+    */
+    className?: string;
+    /**
+        title of accordion item
+    */
+    title: JSX.Element;
+    /**
+        reduce padding space of accordion item, default: false
+    */
+    fullWidth?: boolean;
+    /**
+        elevate accordion item visually, default: false
+    */
+    elevated?: boolean;
+    /**
+        limit vertical white space to condense content of accordion item, default: false
+    */
+    condensed?: boolean;
+    /**
+        remove border separations from accordion item, default: false
+    */
+    noBorder?: boolean;
+    /**
+        render accordion item in open state, default: false
+    */
+    open?: boolean;
+    // allow other properties
+    [otherProps: string]: any;
+}
+
 function AccordionItem({
     children,
     className = "",
@@ -9,7 +43,7 @@ function AccordionItem({
     condensed = false,
     noBorder = false,
     ...otherProps
-}: any) {
+}: IAccordionItemProps) {
     return (
         <CarbonAccordionItem
             className={
