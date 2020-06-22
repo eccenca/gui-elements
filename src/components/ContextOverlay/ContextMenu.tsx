65e4cd90fcd61282b22c91723ee0a8471fb7b92b
Michael Haschke <mhaschke@brox.de>
2020-06-22 16:02:47 +0200

    add interface description to ContextMenu element

--- src/components/ContextOverlay/ContextMenu.tsx
+++ src/components/ContextOverlay/ContextMenu.tsx
@@ -3,18 +3,33 @@ import ContextOverlay from "./ContextOverlay";
 import Menu from "../Menu/Menu";
 import IconButton from "../Icon/IconButton";
 
+interface IContextMenuProps {
+    children: any;
+    /**
+        space-delimited list of class names
+    */
+    className?: string;
+    /**
+        defines toggler elements that is used to open the context menu, could
+        be any element or just a string for the icon name, default: "item-moremenu"
+    */
+    togglerElement?: JSX.Element | string;
+    /**
+        text that is used a tooltip to the toggler, default: "Show more options"
+    */
+    togglerText?: string;
+}
+
 function ContextMenu({
     children,
     className = "",
     togglerElement = "item-moremenu",
     togglerText = "Show more options",
-    togglerLarge = false,
-    ...restProps
-}: any) {
+}: IContextMenuProps) {
     return (
-        <ContextOverlay {...restProps} className={"ecc-contextmenu " + className}>
+        <ContextOverlay className={"ecc-contextmenu " + className}>
             {typeof togglerElement === "string" ? (
-                <IconButton name={togglerElement} text={togglerText} large={togglerLarge} />
+                <IconButton name={togglerElement} text={togglerText} />
             ) : (
                 { togglerElement }
             )}
--- src/components/ContextOverlay/ContextOverlay.tsx
+++ src/components/ContextOverlay/ContextOverlay.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Popover as BlueprintPropover, Position as BlueprintPosition } from "@blueprintjs/core";
+import {
+    Popover as BlueprintPropover,
+    IPopoverProps as IBlueprintPropoverProps,
+    Position as BlueprintPosition
+} from "@blueprintjs/core";
 
 /*
-
     @see https://blueprintjs.com/docs/#core/components/popover for list of
     properties
-
 */
+
+interface IContextOverlayProps extends IBlueprintPropoverProps {
+    children?: any;
+}
 
 function ContextOverlay({
     children,
     className='',
     ...restProps
-}: any) {
+}: IContextOverlayProps) {
 
     return (
         <BlueprintPropover
