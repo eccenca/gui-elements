f9f1e8e13daac47e17f7bc8776fe5038e5493f76
Michael Haschke <michael.haschke@eccenca.com>
2021-03-29 16:02:56 +0200

    add instant replacement for legacy Button element

---
+++ legacy-replacements.ts
@@ -0,0 +1,13 @@
+import { ButtonReplacement as Button } from "./src/legacy-replacements/Button/Button";
+import { AffirmativeButtonReplacement as AffirmativeButton } from "./src/legacy-replacements/Button/AffirmativeButton";
+import { DismissiveButtonReplacement as DismissiveButton } from "./src/legacy-replacements/Button/DismissiveButton";
+import { DisruptiveButtonReplacement as DisruptiveButton } from "./src/legacy-replacements/Button/DisruptiveButton";
+import { ButtonReplacement as ProgressButton } from "./src/legacy-replacements/Button/Button";
+
+export {
+    Button,
+    AffirmativeButton,
+    DismissiveButton,
+    DisruptiveButton,
+    ProgressButton,
+};
---
+++ src/legacy-replacements/Button/Button.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Button from "../../components/Button/Button";
+
+export const iconMappings = {
+    "edit": "item-edit",
+    "delete": "item-remove",
+    "expand_more": "toggler-showmore",
+    "expand_less": "toggler-showless",
+    "arrow_nextpage": "navigation-forth",
+    "arrow_back": "navigation-back"
+};
+
+export function ButtonReplacement ({
+    children,
+    className,
+    fabSize,
+    iconName,
+    tooltip,
+    progress,
+    disabled = false,
+    affirmative = false,
+    dismissive = true,
+    disruptive = false,
+    raised = false,
+    colored = false,
+    ...otherProps
+}: any) {
+    if (process.env.NODE_ENV === 'development') {
+        const debugMsg = ["This button element is a adhoc replacement for a legacy element. Usage is deprecated, please use a standard element (Button)."];
+        if (typeof otherProps.accent !== "undefined") {
+            debugMsg.push("Button 'accent' propertyis not supported on legacy replacement element.");
+            delete otherProps.accent;
+        }
+        if (typeof otherProps.badge !== "undefined") {
+            debugMsg.push("Button 'badge' property is not supported on legacy replacement element.");
+            delete otherProps.badge;
+        }
+        if (typeof otherProps.ripple !== "undefined") {
+            debugMsg.push("Button 'ripple' property is not supported on legacy replacement element.");
+            delete otherProps.ripple;
+        }
+        if (typeof progress !== "undefined") {
+            debugMsg.push("Button 'progress' property is not fully supported on legacy replacement element, it only shows a loading spinner in the button.");
+        }
+        debugMsg.forEach(element => console.debug(element));
+    }
+    return (
+        <Button
+            {...otherProps}
+            className={className}
+            minimal={!!iconName && raised === false ? true : false}
+            icon={iconName ? (iconName && iconMappings[iconName] ? iconMappings[iconName] : iconName) : undefined}
+            tooltip={!!tooltip ? tooltip : false}
+            disabled={disabled}
+            affirmative={affirmative}
+            disruptive={disruptive}
+            hasStatePrimary={ !!fabSize || colored || !dismissive}
+            large={!!fabSize && fabSize !== "mini"}
+            loading={!!progress ? true : false}
+        >
+            {children}
+        </Button>
+    );
+}
