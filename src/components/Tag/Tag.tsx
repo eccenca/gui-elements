46dc8f55d518491da30621ef76ad3818770e2f56
Michael Haschke <michael.haschke@eccenca.com>
2021-06-03 17:41:48 +0200

    add property to set an emphasis level, old emphasized property is now deprecated

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
-import { Tag as BlueprintTag } from "@blueprintjs/core";
+import {
+    Tag as BlueprintTag,
+    ITagProps as IBlueprintTagPRops
+} from "@blueprintjs/core";
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
+
+interface TagProps extends IBlueprintTagPRops {
+    emphasis?: "stronger" | "strong" | "normal" | "weak" | "weaker";
+    // currently not supported
+    active?: never;
+    fill?: never;
+    large?: never;
+    multiline?: never;
+    intent?: never;
+    rightIcon?: never;
+    // deprecated
+    emphasized?: never; // use minimal=false plus emphasis="stronger"
+}
 
 function Tag({
     children,
     className = '',
-    emphasized = false,
+    emphasis = "normal",
+    minimal = true,
     ...otherProps
-}: any) {
+}: TagProps) {
     return (
         <BlueprintTag
             {...otherProps}
             className={
-                `${eccgui}-tag__item` +
+                `${eccgui}-tag__item ${eccgui}-tag--${emphasis}emphasis` +
                 (className ? ' ' + className : '')
             }
-            minimal={!emphasized}
+            minimal={minimal}
         >
             {children}
         </BlueprintTag>
