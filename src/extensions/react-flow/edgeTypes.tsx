dbfec2bbb86394f5917b1df7919ecfde0ffe6d35
Michael Haschke <michael.haschke@eccenca.com>
2021-06-16 11:31:26 +0200

    improve node and handle representations

---
+++ src/extensions/react-flow/edgeTypes.tsx
@@ -0,0 +1,7 @@
+// TODO: we currently cannot fetch elements from thos TS files in node_modules
+// import StraightEdge from "react-flow-renderer/dist/components/Edges/StraightEdge";
+
+export const edgeTypes = {
+    // default: StraightEdge,
+    // step: SmoothStepEdge,
+};
