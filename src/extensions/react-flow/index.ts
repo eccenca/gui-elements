349f39e2c791da59e958d41944800e32d77e322d
darausi <daniel.arausi@eccenca.com>
2021-06-23 08:35:25 +0100

    changed to ts file and exported types correctly

---
+++ src/extensions/react-flow/index.ts
@@ -0,0 +1,6 @@
+import { NodeProps, NodeRectangular } from "./NodeRectangular";
+import { nodeTypes, minimapNodeClassName } from "./nodeTypes";
+import { edgeTypes } from "./edgeTypes";
+
+export type { NodeProps } from "./NodeRectangular";
+export { NodeRectangular, nodeTypes, minimapNodeClassName, edgeTypes };
