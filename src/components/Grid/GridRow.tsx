6a79d1a2108d7262b17b50177afa4ba8d5012ab1
Michael Haschke <michael.haschke@eccenca.com>
2021-07-05 18:08:08 +0200

    extend grids with vertical stretching option

--- src/components/Grid/GridRow.tsx
+++ src/components/Grid/GridRow.tsx
@@ -3,13 +3,21 @@ import React from "react";
 import { Row as CarbonRow } from "carbon-components-react/lib/components/Grid";
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
 
-function GridRow({ children, className = "", dontWrapColumns = true, fullHeight = false, ...otherProps }: any) {
+function GridRow({
+    children,
+    className = "",
+    dontWrapColumns = true,
+    fullHeight = false,
+    verticalStretched,
+    ...otherProps
+}: any) {
     return (
         <CarbonRow
             {...otherProps}
             className={
                 `${eccgui}-grid__row` +
                 (dontWrapColumns ? ` ${eccgui}-grid__row--dontwrapcolumns` : "") +
+                (verticalStretched ? ` ${eccgui}-grid__row--stretched` : "") +
                 (fullHeight ? ` ${eccgui}-grid__row--fullheight` : "") +
                 (className ? " " + className : "")
             }
