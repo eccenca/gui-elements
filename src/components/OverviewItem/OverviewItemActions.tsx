0b46c0af1ddfee6776a45e2d9e05a50281684eba
Michael Haschke <michael.haschke@eccenca.com>
2021-09-16 18:21:19 +0200

    add option to show actions only when there is an interaction (e.g. hovering) with the overview item

--- src/components/OverviewItem/OverviewItemActions.tsx
+++ src/components/OverviewItem/OverviewItemActions.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
 
+interface OverviewItemActionsProps extends React.HTMLAttributes<HTMLDivElement> {
+    className?: string;
+    hiddenInteractions?: boolean;
+}
+
 function OverviewItemActions ({
     children,
     className = '',
+    hiddenInteractions = false,
     ...restProps
-}: any) {
+}: OverviewItemActionsProps) {
     return (
-        <div {...restProps} className={`${eccgui}-overviewitem__actions `+className}>
+        <div
+            {...restProps}
+            className={
+                `${eccgui}-overviewitem__actions` +
+                (hiddenInteractions ? ` ${eccgui}-overviewitem__actions--hiddeninteractions ` : '') +
+                (!!className ? ` ${className}` : '')
+            }
+        >
             {children}
         </div>
     )
