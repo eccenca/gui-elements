7eb840503ad99f7226af792f7a6cea9ca5f02693
Andreas Schultz <andreas.schultz@eccenca.com>
2021-10-05 15:56:29 +0200

    Add cmem module for exports of CMEM related components

---
+++ cmem.ts
@@ -0,0 +1,7 @@
+import ContentBlobToggler from "./src/cmem/ContentBlobToggler";
+import MarkdownParser from "@gui-elements/src/cmem/markdown";
+
+export {
+    ContentBlobToggler,
+    MarkdownParser
+}
