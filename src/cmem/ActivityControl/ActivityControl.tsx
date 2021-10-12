2689b2523f61c47b837247e7e6813cda8531fa05
Michael Haschke <michael.haschke@eccenca.com>
2021-10-12 18:48:10 +0200

    add basic styles to activity control widget, move progress bar out of the line layout

--- src/cmem/ActivityControl/ActivityControl.tsx
+++ src/cmem/ActivityControl/ActivityControl.tsx
@@ -7,13 +7,16 @@ import {
     OverflowText,
     OverviewItem,
     OverviewItemActions,
+    // OverviewItemDepiction,
     OverviewItemDescription,
     OverviewItemLine,
     ProgressBar,
+    // Spinner,
     Tooltip,
 } from "../../../index";
+import {CLASSPREFIX as eccgui} from "../../configuration/constants";
 import {TestableComponent} from "../../components/interfaces";
-import { ProgressBarProps } from "@blueprintjs/core";
+import { ProgressBarProps } from "../../components/ProgressBar/ProgressBar";
 
 export interface IActivityControlProps extends TestableComponent {
     // The label to be shown
@@ -53,63 +56,71 @@ export interface IActivityMenuAction extends IActivityAction, TestableComponent 
 export function ActivityControl(props: IActivityControlProps) {
     const {"data-test-id": dataTestId, progress, activityActions, activityContextMenu} = props
 
-    return <Card><OverviewItem data-test-id={dataTestId}>
-        <OverviewItemDescription>
-            {props.label && <OverviewItemLine>
-                {props.label}
-            </OverviewItemLine>}
-            <OverviewItemLine>
+    /* TODO, add spinner option
+    <OverviewItemDepiction>
+        <Spinner position="inline" value={progress?.value} intent={progress?.intent}/>
+    </OverviewItemDepiction>
+    */
+    return (
+        <div className={`${eccgui}-addon-activitycontrol`}>
+        <Card isOnlyLayout>
+            <OverviewItem data-test-id={dataTestId} hasSpacing>
                 {progress && <ProgressBar
                     intent={"success"}
                     animate={false}
                     stripes={false}
                     {...progress}
                 />}
-            </OverviewItemLine>
-            {props.statusMessage && (
-                <OverviewItemLine>
-                    {
-                        props.statusMessage.length > 50 ? (
-                            <Tooltip content={props.statusMessage}>
-                                <OverflowText inline={true}>
-                                    {props.statusMessage}
-                                </OverflowText>
-                            </Tooltip>
-                        ) : (
-                            <OverflowText inline={true}>
-                                {props.statusMessage}
-                            </OverflowText>
-                        )
+                <OverviewItemDescription>
+                    {props.label && <OverviewItemLine>
+                        {props.label}
+                    </OverviewItemLine>}
+                    {props.statusMessage && (
+                        <OverviewItemLine small>
+                            {
+                                props.statusMessage.length > 50 ? (
+                                    <Tooltip content={props.statusMessage}>
+                                        <OverflowText inline={true}>
+                                            {props.statusMessage}
+                                        </OverflowText>
+                                    </Tooltip>
+                                ) : (
+                                    <OverflowText inline={true}>
+                                        {props.statusMessage}
+                                    </OverflowText>
+                                )
+                            }
+                        </OverviewItemLine>
+                    )}
+                </OverviewItemDescription>
+                <OverviewItemActions>
+                    {activityActions && activityActions.map((action) => {
+                        return <IconButton
+                            key={action.icon}
+                            data-test-id={action["data-test-id"]}
+                            name={action.icon}
+                            text={action.tooltip}
+                            onClick={action.action}
+                            tooltipOpenDelay={200}
+                        />
+                    })}
+                    {activityContextMenu && activityContextMenu.menuItems.length > 0 && <ContextMenu
+                        data-test-id={activityContextMenu["data-test-id"]}
+                        togglerText={activityContextMenu.tooltip}
+                    >
+                        {activityContextMenu.menuItems.map((menuAction) => {
+                            return <MenuItem
+                                icon={menuAction.icon}
+                                key={menuAction.icon}
+                                onClick={menuAction.action}
+                                text={menuAction.tooltip}
+                            />
+                        })}
+                    </ContextMenu>
                     }
-                </OverviewItemLine>
-            )}
-        </OverviewItemDescription>
-        <OverviewItemActions>
-            {activityActions && activityActions.map((action) => {
-                return <IconButton
-                    key={action.icon}
-                    data-test-id={action["data-test-id"]}
-                    name={action.icon}
-                    text={action.tooltip}
-                    onClick={action.action}
-                    tooltipOpenDelay={200}
-                />
-            })}
-            {activityContextMenu && activityContextMenu.menuItems.length > 0 && <ContextMenu
-                data-test-id={activityContextMenu["data-test-id"]}
-                togglerText={activityContextMenu.tooltip}
-            >
-                {activityContextMenu.menuItems.map((menuAction) => {
-                    return <MenuItem
-                        icon={menuAction.icon}
-                        key={menuAction.icon}
-                        onClick={menuAction.action}
-                        text={menuAction.tooltip}
-                    />
-                })}
-            </ContextMenu>
-            }
-        </OverviewItemActions>
-    </OverviewItem>
-    </Card>
+                </OverviewItemActions>
+            </OverviewItem>
+        </Card>
+        </div>
+    );
 }
