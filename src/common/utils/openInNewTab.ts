1868d71a641a0c38ed8269a91f5610168b13f88b
Andreas Schultz <a.schultz@fu-berlin.de>
2021-12-10 10:59:25 +0100

    Check if handler is defined in openInNewTab

--- src/common/utils/openInNewTab.ts
+++ src/common/utils/openInNewTab.ts
@@ -1,3 +1,5 @@
+/** Wrapper for an onClick handler on link-like components, i.e. that navigate to a different URL on click.
+ * Opens a link (url) in a new tab when CMD or CTRL key is pressed at click time. */
 export const openInNewTab = (event, handler, url?:string) => {
   //if ctrl key or cmd key bypass click handler and delegation open in the background new tab
   if (url && (event.ctrlKey || event.metaKey)) {
@@ -5,7 +7,7 @@ export const openInNewTab = (event, handler, url?:string) => {
     event.nativeEvent.stopImmediatePropagation();
     event.stopPropagation();
     window.open(url, "_blank");
-  } else {
+  } else if(handler) {
     handler(event);
   }
 };
