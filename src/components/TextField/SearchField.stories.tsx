263ffd2260ff126beec335782422916640103554
darausi <daniel.arausi@eccenca.com>
2022-01-24 15:33:28 +0100

    disabled some fields for searchfield story

--- src/components/TextField/SearchField.stories.tsx
+++ src/components/TextField/SearchField.stories.tsx
@@ -5,10 +5,26 @@ import CustomSearchField from "./SearchField";
 
 type SearchFieldType = typeof CustomSearchField;
 
+const disabledArgTypes = [
+  "hasStatePrimary",
+  "hasStateDanger",
+  "hasStateWarning",
+  "hasStateSuccess",
+  "fullWidth",
+].reduce((args, field) => {
+  args[field] = {
+    control: {
+      disable: true,
+    },
+  };
+  return args;
+}, {});
 export default {
   title: "Components/SearchField",
   component: CustomSearchField,
-  argTypes: {},
+  argTypes: {
+    ...disabledArgTypes,
+  },
 } as ComponentMeta<SearchFieldType>;
 
 const Template: ComponentStory<SearchFieldType> = (args) => (
@@ -22,4 +38,18 @@ Default.args = {
   leftIcon: "operation-search",
   onClearanceText: "",
   onClearanceHandler: () => {},
+};
+
+export const SearchFieldWithClearanceIcon: ComponentStory<SearchFieldType> = (
+  args
+) => {
+  const [query, setQuery] = React.useState<string>("");
+  return (
+    <CustomSearchField
+      {...args}
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onClearanceHandler={() => setQuery("")}
+    ></CustomSearchField>
+  );
 };
