91ba2dbd3c9fd2903a1698e89c9718230702bcce
darausi <daniel.arausi@eccenca.com>
2022-01-24 17:01:10 +0100

    made corrections to toolbar component

--- src/components/Toolbar/Toolbar.stories.tsx
+++ src/components/Toolbar/Toolbar.stories.tsx
@@ -5,35 +5,33 @@ import CustomToolbar from "./Toolbar";
 import CustomToolbarSection from "./ToolbarSection";
 import Button from "../Button/Button";
 import Spacing from "../Separation/Spacing";
+import SearchField from "../TextField/SearchField";
 
 type ToolbarType = typeof CustomToolbar;
 
 export default {
   title: "Components/Toolbar",
   component: CustomToolbar,
+  subcomponents: {
+    CustomToolbarSection,
+  },
   argTypes: {},
 } as ComponentMeta<ToolbarType>;
 
-const ToolbarSectionTemplate = ({ children, ...otherArgs }) => (
-  <CustomToolbarSection {...otherArgs}>{children}</CustomToolbarSection>
-);
-
-export const ToolbarSection = ToolbarSectionTemplate.bind({});
-
 const Template: ComponentStory<ToolbarType> = (args) => (
   <CustomToolbar {...args}>
-    <ToolbarSectionTemplate>
+    <CustomToolbarSection>
       <Button
         elevated
         text="PrimaryButton"
         hasStatePrimary
         onClick={() => {}}
       />
-    </ToolbarSectionTemplate>
-    <ToolbarSectionTemplate>
+    </CustomToolbarSection>
+    <CustomToolbarSection>
       <Spacing size="tiny" vertical />
       <Button text="Danger Button" hasStateDanger onClick={() => {}} />
-    </ToolbarSectionTemplate>
+    </CustomToolbarSection>
   </CustomToolbar>
 );
 
