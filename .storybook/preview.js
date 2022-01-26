d28c1058341533a60b955c5cf70a072ad9aa2a77
Michael Haschke <michael.haschke@eccenca.com>
2022-01-26 14:00:16 +0100

    disable jest integration as it does not work properly right now

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,15 @@
 import { withTests } from "@storybook/addon-jest";
-import results from "../.jest-test-results.json";
+//import results from "../.jest-test-results.json";
 
 import "../index.scss";
 
+/*
 export const decorators = [
   withTests({
     results,
   }),
 ];
+*/
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
