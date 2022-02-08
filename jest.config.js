e82a6ff44c0cf5d8b93615091b33edd25180c781
Andreas Schultz <andreas.schultz@eccenca.com>
2022-02-08 11:16:11 +0100

    Fix test execution

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  verbose: true,
-  modulePathIgnorePatterns: ["node_modules", "jest-test-results.json"],
-  testEnvironment: "jest-environment-jsdom",
-};
-
-module.exports = config;
