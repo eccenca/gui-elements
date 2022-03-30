535cbab10e4b3a96976dfb265da14e62544a5dd1
Michael Haschke <michael.haschke@eccenca.com>
2022-03-30 16:36:37 +0200

    fix import path

--- src/components/AutoSuggestion/AutoSuggestionList.tsx
+++ src/components/AutoSuggestion/AutoSuggestionList.tsx
@@ -11,7 +11,7 @@ import {
     Spinner,
     Spacing,
     Tooltip,
-} from "gui-elements";
+} from "./../../../";
 import { ISuggestionWithReplacementInfo } from "./AutoSuggestion";
 
 export interface IDropdownProps {
