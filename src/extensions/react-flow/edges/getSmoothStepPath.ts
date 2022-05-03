9019450d69026e45331fdf0a77017ac29bd31100
Michael Haschke <michael.haschke@eccenca.com>
2022-05-03 16:11:41 +0200

    adjust path correction to prevent hidden path starts/ends

--- src/extensions/react-flow/edges/getSmoothStepPath.ts
+++ src/extensions/react-flow/edges/getSmoothStepPath.ts
@@ -11,108 +11,108 @@ import { getEdgeCenter as getCenter, Position } from "react-flow-renderer";
 // from bottom to the left and "leftBottomCorner" goes from left to the bottom.
 // We have to consider the direction of the paths because of the animated lines.
 const bottomLeftCorner = (x: number, y: number, size: number): string =>
-  `L ${x},${y - size}Q ${x},${y} ${x + size},${y}`;
+    `L ${x},${y - size}Q ${x},${y} ${x + size},${y}`;
 const leftBottomCorner = (x: number, y: number, size: number): string =>
-  `L ${x + size},${y}Q ${x},${y} ${x},${y - size}`;
+    `L ${x + size},${y}Q ${x},${y} ${x},${y - size}`;
 const bottomRightCorner = (x: number, y: number, size: number): string =>
-  `L ${x},${y - size}Q ${x},${y} ${x - size},${y}`;
+    `L ${x},${y - size}Q ${x},${y} ${x - size},${y}`;
 const rightBottomCorner = (x: number, y: number, size: number): string =>
-  `L ${x - size},${y}Q ${x},${y} ${x},${y - size}`;
+    `L ${x - size},${y}Q ${x},${y} ${x},${y - size}`;
 const leftTopCorner = (x: number, y: number, size: number): string => `L ${x + size},${y}Q ${x},${y} ${x},${y + size}`;
 const topLeftCorner = (x: number, y: number, size: number): string => `L ${x},${y + size}Q ${x},${y} ${x + size},${y}`;
 const topRightCorner = (x: number, y: number, size: number): string => `L ${x},${y + size}Q ${x},${y} ${x - size},${y}`;
 const rightTopCorner = (x: number, y: number, size: number): string => `L ${x - size},${y}Q ${x},${y} ${x},${y + size}`;
 
 export interface GetSmoothStepPathParams {
-  sourceX: number;
-  sourceY: number;
-  sourcePosition?: Position;
-  targetX: number;
-  targetY: number;
-  targetPosition?: Position;
-  borderRadius?: number;
-  centerX?: number;
-  centerY?: number;
+    sourceX: number;
+    sourceY: number;
+    sourcePosition?: Position;
+    targetX: number;
+    targetY: number;
+    targetPosition?: Position;
+    borderRadius?: number;
+    centerX?: number;
+    centerY?: number;
 }
 
 export function getSmoothStepPath({
-  sourceX,
-  sourceY,
-  sourcePosition = Position.Bottom,
-  targetX,
-  targetY,
-  targetPosition = Position.Top,
-  borderRadius = 5,
-  centerX,
-  centerY,
+    sourceX,
+    sourceY,
+    sourcePosition = Position.Bottom,
+    targetX,
+    targetY,
+    targetPosition = Position.Top,
+    borderRadius = 5,
+    centerX,
+    centerY,
 }: GetSmoothStepPathParams): string {
-  const [_centerX, _centerY, offsetX, offsetY] = getCenter({ sourceX, sourceY, targetX, targetY });
-  const cornerWidth = Math.min(borderRadius, Math.abs(targetX - sourceX));
-  const cornerHeight = Math.min(borderRadius, Math.abs(targetY - sourceY));
-  const cornerSize = Math.min(cornerWidth, cornerHeight, offsetX, offsetY);
-  const leftAndRight = [Position.Left, Position.Right];
-  const cX = typeof centerX !== 'undefined' ? centerX : _centerX;
-  const cY = typeof centerY !== 'undefined' ? centerY : _centerY;
+    const [_centerX, _centerY, offsetX, offsetY] = getCenter({ sourceX, sourceY, targetX, targetY });
+    const cornerWidth = Math.min(borderRadius, Math.abs(targetX - sourceX));
+    const cornerHeight = Math.min(borderRadius, Math.abs(targetY - sourceY));
+    const cornerSize = Math.min(cornerWidth, cornerHeight, offsetX, offsetY);
+    const cX = typeof centerX !== 'undefined' ? centerX : _centerX;
+    const cY = typeof centerY !== 'undefined' ? centerY : _centerY;
 
-  let firstCornerPath = null;
-  let secondCornerPath = null;
+    let firstCornerPath = "";
+    let secondCornerPath = "";
 
-  if (sourceX <= targetX) {
-    firstCornerPath =
-      sourceY <= targetY ? bottomLeftCorner(sourceX, cY, cornerSize) : topLeftCorner(sourceX, cY, cornerSize);
-    secondCornerPath =
-      sourceY <= targetY ? rightTopCorner(targetX, cY, cornerSize) : rightBottomCorner(targetX, cY, cornerSize);
-  } else {
-    firstCornerPath =
-      sourceY < targetY ? bottomRightCorner(sourceX, cY, cornerSize) : topRightCorner(sourceX, cY, cornerSize);
-    secondCornerPath =
-      sourceY < targetY ? leftTopCorner(targetX, cY, cornerSize) : leftBottomCorner(targetX, cY, cornerSize);
-  }
-
-  if (leftAndRight.includes(sourcePosition) && leftAndRight.includes(targetPosition)) {
     if (sourceX <= targetX) {
-      firstCornerPath =
-        sourceY <= targetY ? rightTopCorner(cX, sourceY, cornerSize) : rightBottomCorner(cX, sourceY, cornerSize);
-      secondCornerPath =
-        sourceY <= targetY ? bottomLeftCorner(cX, targetY, cornerSize) : topLeftCorner(cX, targetY, cornerSize);
-    } /*else if (
-      (sourcePosition === Position.Right && targetPosition === Position.Left) ||
-      (sourcePosition === Position.Left && targetPosition === Position.Right) ||
-      (sourcePosition === Position.Left && targetPosition === Position.Left)
-    ) {
-      // and sourceX > targetX
-      firstCornerPath =
-        sourceY <= targetY ? leftTopCorner(cX, sourceY, cornerSize) : leftBottomCorner(cX, sourceY, cornerSize);
-      secondCornerPath =
-        sourceY <= targetY ? bottomRightCorner(cX, targetY, cornerSize) : topRightCorner(cX, targetY, cornerSize);
-    }*/
-  } else if (leftAndRight.includes(sourcePosition) && !leftAndRight.includes(targetPosition)) {
-    if (sourceX <= targetX) {
-      firstCornerPath =
-        sourceY <= targetY
-          ? rightTopCorner(targetX, sourceY, cornerSize)
-          : rightBottomCorner(targetX, sourceY, cornerSize);
+        if (sourceY <= targetY) {
+            firstCornerPath = bottomLeftCorner(sourceX, cY, cornerSize);
+            secondCornerPath = rightTopCorner(targetX, cY, cornerSize);
+            if (sourcePosition === Position.Right) {
+                if (targetPosition === Position.Left) {
+                    firstCornerPath = rightTopCorner(cX, sourceY, cornerSize);
+                    secondCornerPath = bottomLeftCorner(cX, targetY, cornerSize);
+                }
+            } else {
+                if (targetPosition === Position.Left) {
+                    // no change
+                }
+            }
+        } else {
+            firstCornerPath = topLeftCorner(sourceX, cY, cornerSize);
+            secondCornerPath = rightBottomCorner(targetX, cY, cornerSize);
+            if (sourcePosition === Position.Right) {
+                if (targetPosition === Position.Left) {
+                    firstCornerPath = rightBottomCorner(cX, sourceY, cornerSize);
+                    secondCornerPath = topLeftCorner(cX, targetY, cornerSize);
+                }
+            } else {
+                if (targetPosition === Position.Left) {
+                    // no change
+                }
+            }
+        }
     } else {
-      firstCornerPath =
-        sourceY <= targetY
-          ? leftTopCorner(targetX, sourceY, cornerSize)
-          : leftBottomCorner(targetX, sourceY, cornerSize);
+        if (sourceY <= targetY) {
+            firstCornerPath = bottomRightCorner(sourceX, cY, cornerSize);
+            secondCornerPath = leftTopCorner(targetX, cY, cornerSize);
+            if (sourcePosition === Position.Left) {
+                if (targetPosition === Position.Right) {
+                    firstCornerPath = leftTopCorner(cX, sourceY, cornerSize);
+                    secondCornerPath = bottomRightCorner(cX, targetY, cornerSize);
+                }
+            } else {
+                if (targetPosition === Position.Right) {
+                    // no change
+                }
+            }
+        } else {
+            firstCornerPath = topRightCorner(sourceX, cY, cornerSize);
+            secondCornerPath = leftBottomCorner(targetX, cY, cornerSize);
+            if (sourcePosition === Position.Left) {
+                if (targetPosition === Position.Right) {
+                    firstCornerPath = leftBottomCorner(cX, sourceY, cornerSize);
+                    secondCornerPath = topRightCorner(cX, targetY, cornerSize);
+                }
+            } else {
+                if (targetPosition === Position.Right) {
+                    // no change
+                }
+            }
+        }
     }
-    secondCornerPath = '';
-  } else if (!leftAndRight.includes(sourcePosition) && leftAndRight.includes(targetPosition)) {
-    if (sourceX <= targetX) {
-      firstCornerPath =
-        sourceY <= targetY
-          ? bottomLeftCorner(sourceX, targetY, cornerSize)
-          : topLeftCorner(sourceX, targetY, cornerSize);
-    } else {
-      firstCornerPath =
-        sourceY <= targetY
-          ? bottomRightCorner(sourceX, targetY, cornerSize)
-          : topRightCorner(sourceX, targetY, cornerSize);
-    }
-    secondCornerPath = '';
-  }
 
-  return `M ${sourceX},${sourceY}${firstCornerPath}${secondCornerPath}L ${targetX},${targetY}`;
+    return `M ${sourceX},${sourceY}${firstCornerPath}${secondCornerPath}L ${targetX},${targetY}`;
 }
