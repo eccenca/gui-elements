016d1868f3f89e97f6d71f3bb11067ee1cfd4a1b
Michael Haschke <michael.haschke@eccenca.com>
2022-05-30 13:02:33 +0200

    add more documentation about changing the color configuration getters

--- src/common/utils/getColorConfiguration.ts
+++ src/common/utils/getColorConfiguration.ts
@@ -1,7 +1,15 @@
 import CssCustomProperties from "./CssCustomProperties";
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
 
-const getColorConfiguration = (configId: string) => {
+// Configurations can be found in `src/cmem/react-flow/configuration/_colors-*.scss`
+type colorconfigs = "react-flow-graph" | "react-flow-linking" | "react-flow-workflow";
+
+/**
+ * Read and returns color values provided by CSS custom properties.
+ * They are defined for special CSS classes.
+ * Currently color configurations for the react flow editors are supported.
+ **/
+const getColorConfiguration = (configId: colorconfigs) => {
     return new CssCustomProperties({
         selectorText: `.${eccgui}-configuration--colors__${configId}`,
         cssRuleType: "CSSStyleRule",
