da0f0e80d8c696c1dd45e268e4809ca7e057d0aa
Srinivas Chary <srinivasa@aarth.io>
2022-08-21 12:30:38 +0530

    add saperation story in storybook

---
+++ src/components/Separation/Stories/Saperation.stories.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Button from '../../Button/Button'
+import Spacing from '../Spacing'
+export default {
+    title: "Components/Saperation/Spacing",
+    component: Spacing,
+    subcomponents : {Button},
+    argTypes: {
+       size : {control : 'radio' , options : ["tiny" , "small" , "medium" , "large"]},
+       hasDivider : {control : "boolean"},
+       vartical : {control : "boolean"}
+    }
+
+}as ComponentMeta<typeof Spacing>
+// buttons used for only showing space with elements 
+const SpacingExample: ComponentStory<typeof Spacing> = (args) => (
+    <>
+     <Button children="Element"/>
+     <Spacing {...args}/>
+     <Button children="Element"/>
+     <Spacing {...args}/>
+     <Button children="Element"/>
+     <Spacing {...args}/>
+     <Button children="Element"/>
+     <Spacing {...args}/>
+     <Button children="Element"/>
+     <Spacing {...args}/>
+     <Button children="Element"/>
+     
+    </>
+);
+
+
+export const Default  = SpacingExample.bind({});
+Default.args = {
+    size: "tiny",
+    hasDivider: false,
+    vertical: false
+};
+
