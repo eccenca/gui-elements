b8a2a0f82c09a0f057d921ed81e75ca8e0a543ab
Michael Haschke <michael.haschke@eccenca.com>
2022-08-22 11:45:25 +0200

    fix accordion story

--- src/components/Accordion/Stories/Accordion.stories.tsx
+++ src/components/Accordion/Stories/Accordion.stories.tsx
@@ -3,54 +3,27 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Accordion from '../Accordion'
 import AccordionItem from '../AccordionItem';
 
-
 export default {
     title: "Components/Accordion",
     component: Accordion,
     subcomponents : {AccordionItem},
     argTypes: {
-        children : {control : 'text'},
-        disabled: {
-            description: "text content",
-            control: "boolean",
-            table: {
-                defaultValue: { summary: false },
-                type: { summary: "boolean" },
-            }
-        },
-        className: {
-            description: "A space-delimited list of class names.",
-            control: "text",
-            table: {
-                defaultValue: { summary: undefined },
-                type: { summary: "string" },
-            }
-        },
-        onClick: { action: 'clicked' },
-        size : {
-             control: "radio",  options: [ "sm" , "md" , "lg" ] },
-        align : {
-        control: "radio",  options: [ "start"  , "end" ] },
-             noBorder : {control : 'boolean'},
-             state : {control : 'boolean'},
-       
+        children: {
+            control: "none",
+            description: "`<AccordionItem />` elements."
+        }
     }
 
 } as ComponentMeta<typeof Accordion>;
 
 const TemplateIcons: ComponentStory<typeof Accordion> = (args) => (
-    <>
-        <Accordion  {...args}>
-         <AccordionItem  {...args}></AccordionItem>
-        </Accordion>
-    </>
+    <Accordion {...args} />
 );
 export const AccordionElement = TemplateIcons.bind({});
 AccordionElement.args = {
-    children : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    disabled: false,
-    noBorder : false,
-    state : false,
+    children : [
+        <AccordionItem label="Lorem ipsum label">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </AccordionItem>
+    ],
 };
-
-
