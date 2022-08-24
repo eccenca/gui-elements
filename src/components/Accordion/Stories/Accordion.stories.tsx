90e961e28f40d2144cbf311f86aed47ec7054a57
Srinivas Chary <srinivasa@aarth.io>
2022-08-24 15:52:13 +0530

    fix story for accordionItem

--- src/components/Accordion/Stories/Accordion.stories.tsx
+++ src/components/Accordion/Stories/Accordion.stories.tsx
@@ -2,28 +2,27 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Accordion from '../Accordion'
 import AccordionItem from '../AccordionItem';
+import {Default as AccordionStoryItem} from '../Stories/AccordionItem.stories'
 
 export default {
     title: "Components/Accordion",
     component: Accordion,
-    subcomponents : {AccordionItem},
     argTypes: {
         children: {
             control: "none",
-            description: "`<AccordionItem />` elements."
-        }
+            description: "Elements to include into the Accordion component"
+        },
     }
 
 } as ComponentMeta<typeof Accordion>;
 
 const TemplateIcons: ComponentStory<typeof Accordion> = (args) => (
     <Accordion {...args} />
-);
-export const AccordionElement = TemplateIcons.bind({});
-AccordionElement.args = {
+);  
+export const Default = TemplateIcons.bind({});
+Default.args = {
     children : [
-        <AccordionItem label="Lorem ipsum label">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </AccordionItem>
+       <AccordionItem {...AccordionStoryItem.args} />
     ],
 };
+
