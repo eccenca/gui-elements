3bcff5a163709ba5f1ec11a0dfa49b10cc36fa81
Srinivas Chary <srinivasa@aarth.io>
2022-08-25 13:17:02 +0530

    fix menu sub-component story and delete breadcrumb story

--- src/components/Menu/Stories/Menu.stories.tsx
+++ src/components/Menu/Stories/Menu.stories.tsx
@@ -3,47 +3,42 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import Menu from '../Menu'
 import MenuDivider from '../MenuDivider'
 import MenuItem from '../MenuItem'
+import {Default as MenuItemStory} from '../Stories/MenuItem.stories'
 export default {
     title: "Components/Menu",
     component: Menu,
     argTypes: {
-       onClick : {control : 'action'},
-       className : {control : 'string'},
-       large  : {control : "boolean"},
-       multiline :{control : "boolean"},
-       disabled : {control : "boolean"},
-       active : {control : "boolean"},
-       href: {control : 'string'},
+      children: {
+        control: "none",
+        description: "Elements to include into the Menu component"
+    },
+       large  : {
+        control: "boolean",
+        description: "making elements to be large size"
+       },
+       href : {
+        control: "none",
+        description: "access the url"
+       }
     }
 
 }as ComponentMeta<typeof Menu>
 
 const MenuExample: ComponentStory<typeof Menu> = (args) => (
-    <>
-  <Menu  {...args} >
-  <MenuDivider title ="Menu"/>
-   <MenuItem {...args} key="m0" text={"First option"}   icon ={[ "operation-filteredit"]}/>
-        <MenuItem {...args} key="m1" text={"Item two"} icon ={[ "toggler-tree"]}>
-            <MenuItem key="m0" text={"First sub option"} />
-            <MenuItem key="m1" text={"Sub item two"} >
-               <MenuItem key="m0" text={"child sub options"} />
-               <MenuItem key="m1" text={"child sub options"} />
-               <MenuItem key="m2" text={"child sub options"} />
-               <MenuItem key="m3" text={"child sub options"} />
-            </MenuItem>
-        </MenuItem>
-
-  </Menu>
-    </>
+           <Menu  {...args} />
 );
 
 
 export const Default  = MenuExample.bind({});
 Default.args = {
-  className : '',
-  large  : false,
-  multiline :true,
-  disabled : false,
-  active : false,
-  href: " "
+  children : [
+    <>
+     <MenuDivider title ="Menu"/>
+     <MenuItem  key="m0" text={"First option"}   icon ={[ "operation-filteredit"]}/>
+        <MenuItem  key="m1" text={"Item two"} icon ={[ "toggler-tree"]}>
+            <MenuItem key="m0" text={"First sub option"} />
+            <MenuItem key="m1" text={"Second sub items"} {...MenuItemStory.args} />
+      </MenuItem>
+    </>
+  ],
 };
