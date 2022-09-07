4c1e8b7e5e1b2113fdf67d83bf1b6b8440f0cffb
Michael Haschke <michael.haschke@eccenca.com>
2022-09-07 18:42:11 +0200

    provide color type

--- src/common/utils/colorDecideContrastvalue.ts
+++ src/common/utils/colorDecideContrastvalue.ts
@@ -1,12 +1,14 @@
 import Color from "color";
 
+export type colorValue = Color | string;
+
 export interface decideContrastColorValueProps {
     // The color that is used to test if it need a light or dark color as contrast value.
-    testColor: Color | string;
+    testColor: colorValue;
     // the light color that can be used on a dark color
-    lightColor?: Color | string;
+    lightColor?: colorValue;
     // the dark color that can be used on a light color
-    darkColor?: Color | string;
+    darkColor?: colorValue;
 }
 
 const decideContrastColorValue = ({
