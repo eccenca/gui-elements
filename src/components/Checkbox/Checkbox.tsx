8913e8ef3358a9aea08bcd1c0855de7d24a170ba
Michael Haschke <michael.haschke@eccenca.com>
2022-11-03 16:54:24 +0100

    add correct interface

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Checkbox as BlueprintCheckbox } from "@blueprintjs/core";
+import {
+    Checkbox as BlueprintCheckbox,
+    CheckboxProps as BlueprintCheckboxProps
+} from "@blueprintjs/core";
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
+
+interface CheckboxProps extends BlueprintCheckboxProps {
+    // currently we do not alter it
+};
 
 function Checkbox({
     children,
     className='',
     ...restProps
-}: any) {
+}: CheckboxProps) {
     return (
         <BlueprintCheckbox
             {...restProps}
