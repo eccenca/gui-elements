0100d4b31ebce9bda3f2b2a3aa7a43ba9234f08b
Michael Haschke <michael.haschke@eccenca.com>
2022-11-04 12:48:58 +0100

    remove interface descriptions from the stories because this need to be fetched from the interface comments directly

--- src/components/TextField/stories/TextArea.stories.tsx
+++ src/components/TextField/stories/TextArea.stories.tsx
@@ -1,39 +1,24 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
-import CustomTextArea from "./../TextArea";
-
+import { TextArea as CustomTextArea} from "./../../../../index";
 type TextAreaType = typeof CustomTextArea;
 
 export default {
-  title: "Forms/TextArea",
-  component: CustomTextArea,
-  argTypes: {
-    small: {
-      description: "Whether the text area should appear with small styling.",
-      control: "boolean",
+    title: "Forms/TextArea",
+    component: CustomTextArea,
+    argTypes: {
     },
-    large: {
-      description: "Whether the text area should appear with large styling",
-      control: "boolean",
-    },
-    growVertically: {
-      description:
-        "Whether the text area should automatically grow vertically to accommodate content.",
-      control: "boolean",
-    },
-  },
 } as ComponentMeta<TextAreaType>;
 
 const Template: ComponentStory<TextAreaType> = (args) => (
-  <CustomTextArea {...args}></CustomTextArea>
+    <CustomTextArea {...args}></CustomTextArea>
 );
 
 export const Default = Template.bind({});
 Default.args = {
-  hasStatePrimary: false,
-  hasStateSuccess: false,
-  hasStateWarning: false,
-  hasStateDanger: false,
-  rows: 5,
+    hasStatePrimary: false,
+    hasStateSuccess: false,
+    hasStateWarning: false,
+    hasStateDanger: false,
+    rows: 5,
 };
--- src/components/TextField/stories/TextField.stories.tsx
+++ src/components/TextField/stories/TextField.stories.tsx
@@ -1,88 +1,17 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
-import TextField from "./../TextField";
+import { TextField } from "./../../../../index";
 import { helpersArgTypes } from "../../../../.storybook/helpers";
 
 export default {
     title: "Forms/TextField",
     component: TextField,
     argTypes: {
-        className: {
-            description: "A space-delimited list of class names.",
-            control: "boolean",
-            table: {
-                defaultValue: { summary: undefined },
-                type: { summary: "string" },
-            }
-        },
-        value: {
-            description: "Form value of the input, for controlled usage.",
-            control: "text",
-            table: {
-                defaultValue: { summary: undefined },
-                type: { summary: "string" },
-            }
-        },
-        type: {
-            description: "HTML `input` type attribute.",
-            control: "text",
-            table: {
-                defaultValue: { summary: "text" },
-                type: { summary: "string" },
-            }
-        },
-        placeholder: {
-            description: "Placeholder text in the absence of any value.",
-            control: "text",
-            table: {
-                defaultValue: { summary: undefined },
-                type: { summary: "string" },
-            }
-        },
-        disabled: {
-            description: "Whether this action is non-interactive and the button is not usable.",
-            control: "boolean",
-            table: {
-                defaultValue: { summary: false },
-                type: { summary: "boolean" },
-            }
-        },
         leftIcon: {
             ...helpersArgTypes.exampleIcon,
-            description: "Left aligned icon, can be a canonical icon name or an `Icon` element.",
         },
         rightElement: {
             ...helpersArgTypes.exampleIcon,
-            description: "Right aligned element, mainly provided for user-interaction elements, e.g. minimal buttons, spinners, or tags.",
-            //control: { disable: true, },
-            table: {
-                defaultValue: { summary: undefined },
-                type: { summary: "JSX.Element" },
-            }
-        },
-        onChange: {
-            description: "Change event handler. Use `event.target.value` for new value.",
-            table: {
-                defaultValue: { summary: undefined },
-                type: { summary: "React.FormEventHandler<HTMLElement>" },
-            }
-        },
-        large: {
-            description: "Whether this input should use large styles.",
-            control: "boolean",
-            table: {
-                defaultValue: { summary: false },
-                type: { summary: "boolean" },
-            }
-        },
-        small: {
-            description: "Whether this input should use small styles",
-            control: "boolean",
-            table: {
-                defaultValue: { summary: false },
-                type: { summary: "boolean" },
-            }
         },
     },
 } as ComponentMeta<typeof TextField>;
