aa61c581a2ea2ee1fcb2dba12122e5563b823aff
Michael Haschke <michael.haschke@eccenca.com>
2022-11-28 18:17:31 +0100

    output transpiled CSS

--- scripts/compile-sass.ts
+++ scripts/compile-sass.ts
@@ -2,9 +2,11 @@ import * as sass from "sass";
 import tildeImporter from "node-sass-package-importer";
 import sassRenderSyncConfig from "./sassConfig";
 
-sass.renderSync({
+const styles = sass.renderSync({
     importer: tildeImporter(),
     ...sassRenderSyncConfig,
     file: "src/index.scss",
     includePaths: ["node_modules"], // Carbon does not use tilde import syntax
 });
+
+console.log(styles.css.toString());
