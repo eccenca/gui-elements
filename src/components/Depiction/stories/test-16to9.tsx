2c4bf925aba256dd1c50136fb444d143cf806dd0
Michael Haschke <michael.haschke@eccenca.com>
2023-01-17 15:35:31 +0100

    improving test images

--- src/components/Depiction/stories/test-16to9.tsx
+++ src/components/Depiction/stories/test-16to9.tsx
@@ -12,27 +12,27 @@ export const Svg16to9 = () => {
         >
           <g>
             <rect
-               style={{fill:"#ffeeaa", stroke:"none", "stroke-width":"10", "stroke-linecap":"square", "stroke-linejoin":"miter", "stroke-dasharray":"none"}}
+               style={{fill:"#ffeeaa", stroke:"none", strokeWidth:"10", strokeLinecap:"square", strokeLinejoin:"miter", strokeDasharray:"none"}}
                width="1920"
                height="1080"
                x="0"
                y="0" />
             <rect
-               style={{"fill":"#fff6d5", "stroke":"#ffcc00", "stroke-width":"10", "stroke-linecap":"square", "stroke-linejoin":"miter", "stroke-dasharray":"none"}}
+               style={{"fill":"#fff6d5", "stroke":"#ffcc00", strokeWidth:"10", strokeLinecap:"square", strokeLinejoin:"miter", strokeDasharray:"none"}}
                width="1810"
                height="970"
                x="55"
                y="55" />
             <path
-               style={{"fill":"none", "stroke":"#ffcc00", "stroke-width":"10", "stroke-linecap":"butt", "stroke-linejoin":"miter", "stroke-opacity":"1", "stroke-dasharray":"none"}}
+               style={{"fill":"none", "stroke":"#ffcc00", strokeWidth:"10", strokeLinecap:"butt", strokeLinejoin:"miter", strokeOpacity:"1", strokeDasharray:"none"}}
                d="m 55,55 1810,970"
             />
             <path
-               style={{"fill":"none", "stroke":"#ffcc00", "stroke-width":"10", "stroke-linecap":"butt", "stroke-linejoin":"miter", "stroke-opacity":"1", "stroke-dasharray":"none"}}
+               style={{"fill":"none", "stroke":"#ffcc00", strokeWidth:"10", strokeLinecap:"butt", strokeLinejoin:"miter", strokeOpacity:"1", strokeDasharray:"none"}}
                d="M 55,1025 1865,55"
             />
             <text
-               style={{"font-style":"normal", "font-variant":"normal", "font-weight":"normal", "font-stretch":"normal", "font-size":"133.333px", "line-height":"1.25", "font-family":"Roboto", "-inkscape-font-specification":"Roboto"}}
+               style={{fontStyle:"normal", fontVariant:"normal", fontWeight:"normal", fontStretch:"normal", fontSize:"133.333px", lineHeight:"1.25", fontFamily:"Roboto"}}
                x="592.71484"
                y="587.39581"
             ><tspan
--- src/components/Depiction/stories/test-9to16.tsx
+++ src/components/Depiction/stories/test-9to16.tsx
@@ -12,14 +12,14 @@ export const Svg9to16 = () => {
         >
           <g transform="translate(-420,420)">
             <rect
-               style={{fill:"#ffeeaa", stroke:"none", "stroke-width":"10", "stroke-linecap":"square", "stroke-linejoin":"miter", "stroke-dasharray":"none"}}
+               style={{fill:"#ffeeaa", stroke:"none", strokeWidth:"10", strokeLinecap:"square", strokeLinejoin:"miter", strokeDasharray:"none"}}
                width="1920"
                height="1080"
                x="-420"
                y="-1500"
                transform="rotate(90)" />
             <rect
-               style={{"fill":"#fff6d5", "stroke":"#ffcc00", "stroke-width":"10", "stroke-linecap":"square", "stroke-linejoin":"miter", "stroke-dasharray":"none"}}
+               style={{"fill":"#fff6d5", "stroke":"#ffcc00", strokeWidth:"10", strokeLinecap:"square", strokeLinejoin:"miter", strokeDasharray:"none"}}
                id="rect234"
                width="1810"
                height="970"
@@ -27,15 +27,15 @@ export const Svg9to16 = () => {
                y="-1445"
                transform="rotate(90)" />
             <path
-               style={{"fill":"none", "stroke":"#ffcc00", "stroke-width":"10", "stroke-linecap":"butt", "stroke-linejoin":"miter", "stroke-opacity":"1", "stroke-dasharray":"none"}}
+               style={{"fill":"none", "stroke":"#ffcc00", strokeWidth:"10", strokeLinecap:"butt", strokeLinejoin:"miter", strokeOpacity:"1", strokeDasharray:"none"}}
                d="M 1445,-365 475,1445"
                id="path1013" />
             <path
-               style={{"fill":"none", "stroke":"#ffcc00", "stroke-width":"10", "stroke-linecap":"butt", "stroke-linejoin":"miter", "stroke-opacity":"1", "stroke-dasharray":"none"}}
+               style={{"fill":"none", "stroke":"#ffcc00", strokeWidth:"10", strokeLinecap:"butt", strokeLinejoin:"miter", strokeOpacity:"1", strokeDasharray:"none"}}
                d="m 475,-365 970,1810"
                id="path1015" />
             <text
-               style={{"font-style":"normal", "font-variant":"normal", "font-weight":"normal", "font-stretch":"normal", "font-size":"133.333px", "line-height":"1.25", "font-family":"Roboto", "-inkscape-font-specification":"Roboto"}}
+               style={{fontStyle:"normal", fontVariant:"normal", fontWeight:"normal", fontStretch:"normal", fontSize:"133.333px", lineHeight:"1.25", fontFamily:"Roboto"}}
                x="592.71576"
                y="587.39569"
                id="text1797"><tspan
