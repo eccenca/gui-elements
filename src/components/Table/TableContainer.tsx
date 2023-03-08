a80a1de0e72524a93ae06249c78d049b50a311f8
darausi <daniel.arausi@eccenca.com>
2023-03-08 12:13:25 +0100

    corrected formating

--- src/components/Table/TableContainer.tsx
+++ src/components/Table/TableContainer.tsx
@@ -30,13 +30,17 @@ function TableContainer({
 
     const otherDataTableProps = otherProps as TableDataContainerProps;
 
-    return (!!otherDataTableProps.headers || !!otherDataTableProps.rows) ? (
+    return !!otherDataTableProps.headers || !!otherDataTableProps.rows ? (
         <CarbonDataTable.TableContainer className={`${eccgui}-simpletable__container ` + className}>
-        <CarbonDataTable
-            {...otherDataTableProps}
-            size={otherDataTableProps.size ? tableRowHeightSizes[otherDataTableProps.size] : tableRowHeightSizes.medium}
-        />
-    </CarbonDataTable.TableContainer>
+            <CarbonDataTable
+                {...otherDataTableProps}
+                size={
+                    otherDataTableProps.size
+                        ? tableRowHeightSizes[otherDataTableProps.size]
+                        : tableRowHeightSizes.medium
+                }
+            />
+        </CarbonDataTable.TableContainer>
     ) : (
         <CarbonDataTable.TableContainer {...otherProps} className={`${eccgui}-simpletable__container ` + className} />
     );
