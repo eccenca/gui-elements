788c4389f45f7653eeebb42c06ed0ca48bef7187
Andreas Schultz <andreas.schultz@eccenca.com>
2023-06-29 10:40:34 +0200

    Export interface NodeContentHandleNextProps

--- src/extensions/react-flow/nodes/NodeContent.tsx
+++ src/extensions/react-flow/nodes/NodeContent.tsx
@@ -24,7 +24,7 @@ interface NodeContentHandleLegacyProps extends HandleProps {
 // @deprecated use `NodeContentHandleProps`
 export type IHandleProps = NodeContentHandleLegacyProps;
 
-interface NodeContentHandleNextProps extends HandleNextProps {
+export interface NodeContentHandleNextProps extends HandleNextProps {
     category?: "configuration";
 }
 
