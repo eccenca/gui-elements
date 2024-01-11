2faba21821f011e8f2b7b357868bd41e7fd56869
Michael Haschke <michael.haschke@eccenca.com>
2024-01-11 12:46:24 +0100

    add nowrap option to PropertyName element

--- src/components/PropertyValuePair/PropertyName.tsx
+++ src/components/PropertyValuePair/PropertyName.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
 import Label, { LabelProps } from "../Label/Label";
+import OverflowText from "../Typography/OverflowText";
 
 export interface PropertyNameProps extends React.HTMLAttributes<HTMLElement> {
     /**
@@ -9,25 +10,43 @@ export interface PropertyNameProps extends React.HTMLAttributes<HTMLElement> {
      */
     size?: "small" | "medium" | "large";
     /**
+     * Force label to get displayed without line breaks.
+     * This only works best if you use a simple string as content.
+     * Otherwise you may need to take care yourself about it.
+     */
+    nowrap?: boolean;
+    /**
      * Additional label properties, e.g. `tooltip`.
      * It is only used if the `PropertyName` has simple text input.
      */
     labelProps?: LabelProps;
 }
 
-export const PropertyName = ({ className = "", size, children, labelProps, ...otherDtProps }: PropertyNameProps) => {
+export const PropertyName = ({
+    children,
+    className = "",
+    size,
+    nowrap,
+    labelProps,
+    ...otherDtProps
+}: PropertyNameProps) => {
     return (
         <dt
             className={
                 `${eccgui}-propertyvalue__property` +
                 (size ? ` ${eccgui}-propertyvalue__property--${size}` : "") +
+                (nowrap ? ` ${eccgui}-propertyvalue__property--nowrap` : "") +
                 (className ? " " + className : "")
             }
             {...otherDtProps}
         >
             <div>
                 {typeof children === "string" ? (
-                    <Label text={children} isLayoutForElement="span" {...labelProps} />
+                    <Label
+                        text={nowrap ? <OverflowText inline>{children}</OverflowText> : children}
+                        isLayoutForElement="span"
+                        {...labelProps}
+                    />
                 ) : (
                     children
                 )}
