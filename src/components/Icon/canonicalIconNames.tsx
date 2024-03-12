259ded762fc52d5c34dbea0df14c47a3d83043cb
Andreas Schultz <andreas.schultz@eccenca.com>
2024-03-12 12:07:53 +0100

    Add 'Compare' icon

--- src/components/Icon/canonicalIconNames.tsx
+++ src/components/Icon/canonicalIconNames.tsx
@@ -151,6 +151,7 @@ export type ValidIconName =
     | "toggler-star-filled"
     | "toggler-tree"
     | "form-template"
+    | "compare"
     | "undefined";
 
 export type CarbonIconType = React.ComponentType<CarbonIconProps>;
@@ -203,6 +204,8 @@ const canonicalIconNames: Record<ValidIconName, CarbonIconType> = {
     "artefact-transform": icons.DataRefinery,
     "artefact-uncategorized": icons.Unknown,
     "artefact-workflow": icons.ModelBuilder,
+
+    "compare": icons.Compare,
 
     "data-sourcepath": icons.Data_2,
     "data-targetpath": icons.Data_1,
