eff2d4c068b9621390a63e2ddd72e63cb4e183e1
Andreas Schultz <a.schultz@fu-berlin.de>
2024-07-29 12:31:36 +0200

    Generate class attached to body element based on the data transfer type of drag over events

--- src/components/Application/ApplicationContainer.tsx
+++ src/components/Application/ApplicationContainer.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { OverlaysProvider } from "@blueprintjs/core";
 
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
+import {useGlobalAppDragMonitor} from "./helper";
 
 export type ApplicationContainerProps = React.HTMLAttributes<HTMLDivElement>;
+export const APPLICATION_CONTAINER_ID = "gui-elements-application-container"
 
 export const ApplicationContainer = ({ children, className = "", ...otherDivProps }: ApplicationContainerProps) => {
+    useGlobalAppDragMonitor()
+
     return (
         <OverlaysProvider>
-            <div className={`${eccgui}-application__container ${className}`} {...otherDivProps}>
+            <div id={APPLICATION_CONTAINER_ID} className={`${eccgui}-application__container ${className}`} {...otherDivProps}>
                 {children}
             </div>
         </OverlaysProvider>
--- src/components/Application/helper.ts
+++ src/components/Application/helper.ts
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { CLASSPREFIX as eccgui } from "../../configuration/constants";
+import {CLASSPREFIX as eccgui} from "../../configuration/constants";
+import {APPLICATION_CONTAINER_ID} from "./ApplicationContainer";
 
 export const useApplicationHeaderOverModals = (elevate: boolean, className: string) => {
     return React.useEffect(() => {
@@ -18,4 +19,49 @@ export const useApplicationHeaderOverModals = (elevate: boolean, className: stri
             });
         }
     }, [elevate, className]);
+};
+
+// Prefix of the generated class. This will be combined with the data transfer type.
+const APP_DRAG_OVER_CLASS_PREFIX = "appDragOver";
+const nonClassChars = /[^-_a-zA-Z0-9]/g;
+const DRAG_CLASS_REMOVAL_DELAY = 500
+
+/** Tracks drag operations over the application. Sets different classes to the root div. */
+export const useGlobalAppDragMonitor = () => {
+    React.useEffect(() => {
+        const applicationContainer = document.getElementById(APPLICATION_CONTAINER_ID);
+        const body = document.getElementsByTagName("body").item(0)
+        let currentTimer: any = undefined;
+        let currentClass: string | undefined = undefined;
+
+        const removeDragClass = () => {
+            currentClass && body?.classList.remove(currentClass);
+            currentClass = undefined;
+        };
+
+        const onDragOver = (event: DragEvent) => {
+            if (currentTimer) {
+                clearTimeout(currentTimer);
+            }
+            const types = new Set(event.dataTransfer?.types);
+            if (types.size === 1) {
+                const type = types.values().next().value;
+                currentClass = `${APP_DRAG_OVER_CLASS_PREFIX}_${type}`.replaceAll(nonClassChars, "");
+                body?.classList.add(currentClass);
+                currentTimer = setTimeout(removeDragClass, DRAG_CLASS_REMOVAL_DELAY);
+            }
+        };
+
+        const onDrop = () => {
+            if (currentTimer) {
+                clearTimeout(currentTimer);
+            }
+            removeDragClass();
+        };
+
+        if (applicationContainer) {
+            applicationContainer.addEventListener("dragover", onDragOver);
+            applicationContainer.addEventListener("drop", onDrop);
+        }
+    }, []);
 };
