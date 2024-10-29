57c83324592e4783112f424821bcc52336592e41
Michael Haschke <michael.haschke@eccenca.com>
2024-10-29 15:27:27 +0100

    set all legacy support components as deprecated, they are going to removed in v25

--- src/legacy-replacements/Button/DismissiveButton.tsx
+++ src/legacy-replacements/Button/DismissiveButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { ButtonReplacement } from "./Button";
 
+/** @deprecated (v25) all legacy component support will be removed, switch to `<Button dismissive />` */
 export function DismissiveButtonReplacement({ children, ...otherProps }: any) {
     return (
         <ButtonReplacement {...otherProps} dismissive={true}>
