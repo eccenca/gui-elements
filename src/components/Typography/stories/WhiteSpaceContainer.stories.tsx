0c264fb0b865df1536ec03bbee048d1118c3df55
Michael Haschke <michael.haschke@eccenca.com>
2024-11-18 17:58:00 +0100

    add story for HtmlContentBlock

--- src/components/Typography/stories/WhiteSpaceContainer.stories.tsx
+++ src/components/Typography/stories/WhiteSpaceContainer.stories.tsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { loremIpsum } from "react-lorem-ipsum";
 import { Meta, StoryFn } from "@storybook/react";
 
-import { WhiteSpaceContainer as WhiteSpaceContainerElement } from "../../../index";
+import { WhiteSpaceContainer } from "../../../index";
+
+import { Default as HtmlContentBlockStrory } from "./HtmlContentBlock.stories";
 
 const whiteSpaceSizeOptions = {
-    control: "select",
-    options: {
+    control: { type: "select" },
+    options: ["not set", "tiny", "small", "regular", "large", "xlarge"],
+    mapping: {
         "not set": undefined,
-        tiny: "tiny",
-        small: "small",
-        regular: "regular",
-        large: "large",
-        xlarge: "xlarge",
     },
 };
 
 export default {
-    title: "Components/Typography",
-    component: WhiteSpaceContainerElement,
+    title: "Components/Typography/WhiteSpaceContainer",
+    component: WhiteSpaceContainer,
     argTypes: {
-        children: { control: "none" },
+        children: { control: false },
         marginTop: { ...whiteSpaceSizeOptions },
         marginRight: { ...whiteSpaceSizeOptions },
         marginBottom: { ...whiteSpaceSizeOptions },
@@ -30,27 +27,15 @@ export default {
         paddingBottom: { ...whiteSpaceSizeOptions },
         paddingLeft: { ...whiteSpaceSizeOptions },
     },
-} as Meta<typeof WhiteSpaceContainerElement>;
+} as Meta<typeof WhiteSpaceContainer>;
 
-const Template: StoryFn<typeof WhiteSpaceContainerElement> = (args) => (
-    <WhiteSpaceContainerElement {...args} style={{ background: "#eee" }} />
+const Template: StoryFn<typeof WhiteSpaceContainer> = (args) => (
+    <WhiteSpaceContainer {...args} style={{ background: "#eee" }} />
 );
 
-const testContent = loremIpsum({
-    p: 2,
-    avgSentencesPerParagraph: 4,
-    random: false,
-}).toString();
-
-export const WhiteSpaceContainer = Template.bind({});
-WhiteSpaceContainer.args = {
-    children: [
-        <p>{testContent}</p>,
-        <p>
-            <strong>{testContent.replaceAll(" ", "")}</strong>
-        </p>,
-        <p>{testContent}</p>,
-    ],
+export const Default = Template.bind({});
+Default.args = {
+    children: HtmlContentBlockStrory.args.children,
     paddingTop: "tiny",
     paddingRight: "small",
     paddingBottom: "regular",
