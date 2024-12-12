522523e1685aa03d3266a949b07d2142945058c1
Andreas Schultz <a.schultz@fu-berlin.de>
2024-12-12 09:58:04 +0100

    Switch to former solution for initialValue changes in CodeAutoCompletefield

--- src/components/CodeAutocompleteField/CodeAutocompleteField.tsx
+++ src/components/CodeAutocompleteField/CodeAutocompleteField.tsx
@@ -5,8 +5,7 @@ import AutoSuggestion, { AutoSuggestionProps } from "../AutoSuggestion/AutoSugge
 import Spinner from "../Spinner/Spinner";
 
 export interface CodeAutocompleteFieldProps extends AutoSuggestionProps {
-    /** If the component should re-init when the initial value changes. Used when a value should be set externally.*/
-    reInitOnInitialValueChange?: boolean
+    
 }
 
 /**
@@ -15,25 +14,9 @@ export interface CodeAutocompleteFieldProps extends AutoSuggestionProps {
  *
  * Example usage: input of a path string offering auto-completion for each single part of the path.
  */
-export function CodeAutocompleteField({ className, reInitOnInitialValueChange = false, ...otherProps }: CodeAutocompleteFieldProps) {
-    const [reInit, setReInit] = React.useState<boolean>(false)
-
-    React.useEffect(() => {
-        if(reInitOnInitialValueChange) {
-            setReInit(true)
-        }
-    }, [otherProps.initialValue, reInitOnInitialValueChange])
-
-    React.useEffect(() => {
-        if(reInit) {
-            setReInit(false)
-        }
-    }, [reInit])
-
+export function CodeAutocompleteField({ className, ...otherProps }: CodeAutocompleteFieldProps) {
     // Currently this works only as an alias element for `AutoSuggestion`.
-    return reInit ?
-        <Spinner size="tiny" position="inline" description="Validating value path"/> :
-        <AutoSuggestion
+    return <AutoSuggestion
             className={`${eccgui}-codeautocompletefield` + (className ? ` ${className}` : "")}
             {...otherProps}
         />
