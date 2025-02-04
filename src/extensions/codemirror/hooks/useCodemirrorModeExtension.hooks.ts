51908982e403d0fbb6ed0dd77108f238686a4172
arausly <arausid@yahoo.com>
2025-02-04 14:22:09 +0100

    upgraded to available v6 adapated modes for codemirror v6

--- src/extensions/codemirror/hooks/useCodemirrorModeExtension.hooks.ts
+++ src/extensions/codemirror/hooks/useCodemirrorModeExtension.hooks.ts
@@ -1,17 +1,17 @@
+import { javascript } from "@codemirror/lang-javascript";
 import { json } from "@codemirror/lang-json";
 //modes imports
 import { markdown } from "@codemirror/lang-markdown";
+import { python } from "@codemirror/lang-python";
 import { sql } from "@codemirror/lang-sql";
 import { xml } from "@codemirror/lang-xml";
+import { yaml } from "@codemirror/lang-yaml";
 import { defaultHighlightStyle, LanguageSupport, StreamLanguage, StreamParser } from "@codemirror/language";
-import { javascript } from "@codemirror/legacy-modes/mode/javascript";
 import { jinja2 } from "@codemirror/legacy-modes/mode/jinja2";
 import { mathematica } from "@codemirror/legacy-modes/mode/mathematica";
 import { ntriples } from "@codemirror/legacy-modes/mode/ntriples";
-import { python } from "@codemirror/legacy-modes/mode/python";
 import { sparql } from "@codemirror/legacy-modes/mode/sparql";
 import { turtle } from "@codemirror/legacy-modes/mode/turtle";
-import { yaml } from "@codemirror/legacy-modes/mode/yaml";
 
 //adaptations
 import { adaptedSyntaxHighlighting } from "../tests/codemirrorTestHelper";
@@ -31,13 +31,22 @@ const supportedModes = {
     javascript,
 } as const;
 
+const v6AdaptedModes = new Map([
+    ["json", true],
+    ["markdown", true],
+    ["xml", true],
+    ["sql", true],
+    ["yaml", true],
+    ["python", true],
+    ["javascript", true],
+]);
 export const supportedCodeEditorModes = Object.keys(supportedModes) as Array<keyof typeof supportedModes>;
 export type SupportedCodeEditorModes = (typeof supportedCodeEditorModes)[number];
 
 export const useCodeMirrorModeExtension = (mode?: SupportedCodeEditorModes) => {
     return !mode
         ? adaptedSyntaxHighlighting(defaultHighlightStyle)
-        : ["json", "markdown", "xml", "sql"].includes(mode)
+        : v6AdaptedModes.has(mode)
         ? ((typeof supportedModes[mode] === "function" ? supportedModes[mode] : () => {}) as () => LanguageSupport)()
         : StreamLanguage?.define(supportedModes[mode] as StreamParser<unknown>);
 };
