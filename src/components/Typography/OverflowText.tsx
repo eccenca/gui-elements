0cc633c2c3f03593e5a86a8264b609488402eaf4
Michael Haschke <michael.haschke@eccenca.com>
2025-04-02 14:29:53 +0200

    add story and documentation for OverflowText component

--- src/components/Typography/OverflowText.tsx
+++ src/components/Typography/OverflowText.tsx
@@ -3,10 +3,26 @@ import React from "react";
 import { CLASSPREFIX as eccgui } from "../../configuration/constants";
 
 export interface OverflowTextProps {
-    ellipsis?: "reverse" | "none";
+    /**
+     * How is ellipsis used to cut text overflows.
+     * Use `reverse`to use the ellipis on text start and display the end of the text.
+     */
+    ellipsis?: "add" | "reverse" | "none";
+    /**
+     * Display component as inline element.
+     */
     inline?: boolean;
+    /**
+     * Using text overflow on all children elements.
+     */
     passDown?: boolean;
+    /**
+     * Additional CSS class name.
+     */
     className?: string;
+    /**
+     * HTML element that is used for the component.
+     */
     useHtmlElement?: "p" | "div" | "span";
     [key: string]: any;
 }
@@ -15,7 +31,7 @@ export interface OverflowTextProps {
 export const OverflowText = ({
     className = "",
     children,
-    ellipsis,
+    ellipsis = "add",
     inline = false,
     passDown = false,
     useHtmlElement,
