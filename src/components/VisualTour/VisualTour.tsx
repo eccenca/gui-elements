8289d23e9975c21cef0d2a1f04df718522a74998
Andreas Schultz <a.schultz@fu-berlin.de>
2025-08-04 12:07:36 +0200

    Add prev button; fix class names; Refactor to support popover

--- src/components/VisualTour/VisualTour.tsx
+++ src/components/VisualTour/VisualTour.tsx
@@ -15,8 +15,10 @@ export interface VisualTourProps {
     onClose: () => void;
     /** Label of the button to close the tour. */
     closeLabel?: string;
-    /** The label for the next button. */
+    /** The label for the 'next' button. */
     nextLabel?: string;
+    /** The label for the 'previous' button. */
+    prevLabel?: string;
 }
 
 interface VisualTourStep {
@@ -38,6 +40,7 @@ export const VisualTour = ({
     onClose,
     closeLabel = "Close",
     nextLabel = "Next",
+    prevLabel = "Back",
 }: VisualTourProps) => {
     const [currentStepIndex, setCurrentStepIndex] = React.useState<number>(0);
     const [currentStepComponent, setCurrentStepComponent] = React.useState<React.JSX.Element | null>(null);
@@ -51,45 +54,121 @@ export const VisualTour = ({
             return;
         }
         const hasNextStep = currentStepIndex + 1 < steps.length;
+        const hasPreviousStep = currentStepIndex > 0;
         // Configure optional highlighting
         const element = document.querySelector(containerElementQuery);
+        let elementToHighlight: Element | null = null;
         if (element) {
             // Remove previous element highlight
             element.classList.remove(containerHighlightClass);
             document.querySelector(`.${highlightElementClass}`)?.classList.remove(highlightElementClass);
             if (step.highlightElementQuery) {
-                const elementToHighlight = document.querySelector(step.highlightElementQuery);
+                elementToHighlight = document.querySelector(step.highlightElementQuery);
                 if (elementToHighlight) {
                     element.classList.add(containerHighlightClass);
                     elementToHighlight.classList.add(highlightElementClass);
                 }
             }
         }
-        setCurrentStepComponent(
-            <SimpleDialog
-                title={step.title + ` ${currentStepIndex + 1} / ${steps.length}`}
-                isOpen={true}
-                preventSimpleClosing={true}
-                onClose={onClose}
-                actions={[
-                    <Button onClick={onClose}>{closeLabel}</Button>,
-                    hasNextStep ? (
-                        <Button
-                            onClick={() => {
-                                setCurrentStepIndex(currentStepIndex + 1);
-                            }}
-                        >
-                            {nextLabel}: {steps[currentStepIndex + 1].title}
-                        </Button>
-                    ) : null,
-                ]}
-            >
-                {typeof step.content === "string" ? step.content : step.content()}
-            </SimpleDialog>
-        );
+        const titleSuffix = ` ${currentStepIndex + 1} / ${steps.length}`;
+        const navigationButtons = [
+            <Button onClick={onClose}>{closeLabel}</Button>,
+            hasNextStep ? (
+                <Button
+                    onClick={() => {
+                        setCurrentStepIndex(currentStepIndex + 1);
+                    }}
+                >
+                    {nextLabel}: {steps[currentStepIndex + 1].title}
+                </Button>
+            ) : null,
+            hasPreviousStep ? (
+                <Button
+                    onClick={() => {
+                        setCurrentStepIndex(currentStepIndex - 1);
+                    }}
+                >
+                    {prevLabel}: {steps[currentStepIndex - 1].title}
+                </Button>
+            ) : null,
+        ];
+        // TODO: What to do if an element should have been highlighted, but none was found?
+        if (elementToHighlight) {
+            setCurrentStepComponent(
+                <StepPopover
+                    highlightedElement={elementToHighlight}
+                    titleSuffix={titleSuffix}
+                    navigationButtons={navigationButtons}
+                    step={step}
+                    onClose={onClose}
+                />
+            );
+        } else {
+            setCurrentStepComponent(
+                <StepModal
+                    titleSuffix={titleSuffix}
+                    navigationButtons={navigationButtons}
+                    step={step}
+                    onClose={onClose}
+                />
+            );
+        }
     }, [currentStepIndex]);
 
     return currentStepComponent;
+};
+
+interface StepModalProps {
+    step: VisualTourStep;
+    // Current step starting with 1
+    titleSuffix: string;
+    // Close the visual tour
+    onClose: () => void;
+    // The navigation buttons
+    navigationButtons: (React.JSX.Element | null)[];
+}
+
+/** Modal that is displayed for a step. */
+const StepModal = ({ step, titleSuffix, onClose, navigationButtons }: StepModalProps) => {
+    return (
+        <SimpleDialog
+            title={`${step.title} ${titleSuffix}`}
+            isOpen={true}
+            preventSimpleClosing={true}
+            onClose={onClose}
+            actions={navigationButtons}
+        >
+            {typeof step.content === "string" ? step.content : step.content()}
+        </SimpleDialog>
+    );
+};
+
+interface StepPopoverProps {
+    highlightedElement: Element;
+    step: VisualTourStep;
+    // Current step starting with 1
+    titleSuffix: string;
+    // Close the visual tour
+    onClose: () => void;
+    // The navigation buttons
+    navigationButtons: (React.JSX.Element | null)[];
+}
+
+/** Popover that is displayed and points at the highlighted element. */
+const StepPopover = ({ highlightedElement, step, titleSuffix, onClose, navigationButtons }: StepPopoverProps) => {
+    // TODO: Show popover on highlighted element
+    return (
+        <SimpleDialog
+            title={`${step.title} ${titleSuffix}`}
+            isOpen={true}
+            preventSimpleClosing={true}
+            onClose={onClose}
+            actions={navigationButtons}
+        >
+            {typeof step.content === "string" ? step.content : step.content()}
+            {highlightedElement.tagName}
+        </SimpleDialog>
+    );
 };
 
 export default VisualTour;
