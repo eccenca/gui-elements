9003cf4e632c80fbbed7f214ab9f1b8fda4bb793
Andreas Schultz <as.c@mailbox.org>
2025-10-24 13:08:14 +0200

    Change modal open property in modal context to open modal stack
    - In order to track nested modal usages

--- src/components/Dialog/ModalContext.tsx
+++ src/components/Dialog/ModalContext.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-interface ModalContextProps {
+export interface ModalContextProps {
     /** Set that a specific modal is currently being open (or closed) */
     setModalOpen: (modalId: string, isOpen: boolean) => void;
 
-    /** This is true when any modal is currently open. */
-    isOpen: boolean;
+    /** The currently opened modals ordered by when they have been opened. Oldest coming first. */
+    openModalStack: string[] | undefined;
 }
 
 /** Can be provided in the application to react to modal related changes. */
 export const ModalContext = React.createContext<ModalContextProps>({
     setModalOpen: () => {},
-    isOpen: false,
+    openModalStack: [],
 })
