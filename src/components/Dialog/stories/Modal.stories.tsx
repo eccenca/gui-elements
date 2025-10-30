1d4f034e9cd0c8bdefa24b6526b452d3ba6840fb
Michael Haschke <michael.haschke@eccenca.com>
2025-10-30 18:46:24 +0100

    improve display of ModalContext example

--- src/components/Dialog/stories/Modal.stories.tsx
+++ src/components/Dialog/stories/Modal.stories.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { OverlaysProvider } from "@blueprintjs/core";
+import { Classes, OverlaysProvider } from "@blueprintjs/core";
 import { Meta, StoryFn } from "@storybook/react";
 import { fn } from "@storybook/test";
 
 import { SimpleCard } from "../../Card/stories/Card.stories";
-import { ModalContext, useModalContext } from "../ModalContext";
 
-import { Button, Card, CardContent, CardHeader, CardTitle, Modal, Spacing } from "./../../../../index";
+import {
+    Button,
+    Card,
+    CardContent,
+    Modal,
+    ModalContext,
+    ModalSize,
+    Spacing,
+    useModalContext,
+} from "./../../../../index";
 
 export default {
     title: "Components/Dialog/Modal",
@@ -26,20 +34,6 @@ const Template: StoryFn<typeof Modal> = (args) => (
     </OverlaysProvider>
 );
 
-const ContextTemplate: StoryFn<typeof Modal> = (args) => {
-    const { setModalOpen, openModalStack } = useModalContext();
-
-    return (
-        <ModalContext.Provider value={{ setModalOpen, openModalStack }}>
-            <OverlaysProvider>
-                <div style={{ height: "400px" }}>
-                    <Modal {...args} />
-                </div>
-            </OverlaysProvider>
-        </ModalContext.Provider>
-    );
-};
-
 export const Default = Template.bind({});
 Default.args = {
     isOpen: true,
@@ -49,96 +43,105 @@ Default.args = {
     onClosing: fn(),
 };
 
-/** Nested modals with modal context for tracking open/close states. */
-const InnerModal = () => {
-    const [isOpen, setIsOpen] = React.useState(true);
-    const [portalElement, setPortalElement] = React.useState<HTMLElement | undefined>();
-
-    React.useEffect(() => {
-        setPortalElement(document.getElementById("modalPortal"));
-    }, []);
+const ContextTemplate = ({ children }: React.HTMLAttributes<HTMLDivElement>) => {
+    const { setModalOpen, openModalStack } = useModalContext();
 
     return (
-        <Modal
-            isOpen={isOpen}
-            usePortal={true}
-            size={"tiny"}
-            modalId={"innerModal"}
-            portalContainer={portalElement}
-            hasBackdrop={true}
-        >
-            Inner modal with constant modal ID "innerModal".
-            <Spacing />
-            <TrackingContent
-                more={
-                    <>
-                        <Spacing key={"spacing"} />
-                        <Button key={"close"} onClick={() => setIsOpen(false)}>
-                            Close
-                        </Button>
-                    </>
-                }
-            />
-        </Modal>
+        <OverlaysProvider>
+            <div style={{ height: "70vh", position: "relative" }} id={"modalPortal"}>
+                <ModalContext.Provider value={{ setModalOpen, openModalStack }}>{children}</ModalContext.Provider>
+            </div>
+        </OverlaysProvider>
     );
 };
 
-const MiddleModal = () => {
-    const [portalElement, setPortalElement] = React.useState<HTMLElement | undefined>();
-
-    React.useEffect(() => {
-        setPortalElement(document.getElementById("modalPortal"));
-    }, []);
-
+const ModalContent = ({ children }: React.HTMLAttributes<HTMLDivElement>) => {
     return (
-        <Modal
-            isOpen={true}
-            modalId={"middleModal"}
-            size={"small"}
-            usePortal={true}
-            portalContainer={portalElement}
-            hasBackdrop={true}
-        >
-            Middle modal with constant modal ID "middleModal".
-            <Spacing />
-            <TrackingContent more={<InnerModal />} />
-        </Modal>
-    );
-};
-
-/** Shows the current stack of open modals. */
-const TrackingContent = ({ more }: { more: React.JSX.Element }) => {
-    const modalContext = React.useContext(ModalContext);
-
-    return (
-        <Card>
-            <CardHeader key="1">
-                <CardTitle>Tracking content</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <ul>
-                    {(modalContext.openModalStack ?? []).map((modalId, idx) => (
-                        <li key={modalId}>
-                            {idx + 1}. {modalId}
-                        </li>
-                    ))}
-                </ul>
-                {more}
-            </CardContent>
+        <Card style={{ height: "100%" }}>
+            <CardContent>{children}</CardContent>
         </Card>
     );
 };
 
+/** Component for nested modals. */
+const ExampleModal = ({
+    id,
+    size,
+    children,
+}: {
+    id?: string;
+    size: ModalSize;
+    children?: React.HTMLAttributes<HTMLDivElement>["children"];
+}) => {
+    const [isOpen, setIsOpen] = React.useState(true);
+    const [portalElement, setPortalElement] = React.useState<HTMLElement | undefined>();
+
+    React.useEffect(() => {
+        setPortalElement(document.getElementById("modalPortal")!);
+    }, []);
+
+    return (
+        <Modal
+            modalId={id}
+            size={size}
+            isOpen={isOpen}
+            usePortal={true}
+            portalContainer={portalElement}
+            hasBackdrop={true}
+            onOpened={() => {
+                // workaround, Blueprint attach a class to body tht prevents scrolling, probably it is attached to the wrong portal
+                document.body.classList.remove(Classes.OVERLAY_OPEN);
+            }}
+        >
+            <ModalContent>
+                Modal with constant modal ID "{id}".
+                <Spacing />
+                <TrackingContent />
+                <Spacing />
+                {children}
+                <Spacing />
+                <Button key={"close"} onClick={() => setIsOpen(false)}>
+                    Close
+                </Button>
+            </ModalContent>
+        </Modal>
+    );
+};
+
+const InnerModal = () => {
+    return <ExampleModal id="innerModal" size="small" />;
+};
+
+const MiddleModal = () => {
+    return (
+        <ExampleModal id="middleModal" size="regular">
+            <InnerModal />
+        </ExampleModal>
+    );
+};
+
+/** Shows the current stack of open modals. */
+const TrackingContent = () => {
+    const modalContext = React.useContext(ModalContext);
+
+    return (
+        <ul>
+            {(modalContext.openModalStack ?? []).map((modalId, idx) => (
+                <li key={modalId}>
+                    {idx + 1}. {modalId}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+//*
 export const NestedModalWithContext = ContextTemplate.bind({});
 NestedModalWithContext.args = {
-    ...Default.args,
-    size: "regular",
-    usePortal: false,
     children: [
-        <div id={"modalPortal"}>
-            Root modal with automatically generated unique modal ID.
-            <Spacing />
+        <ExampleModal size="large">
             <MiddleModal />
-        </div>,
+        </ExampleModal>,
     ],
 };
+// */
