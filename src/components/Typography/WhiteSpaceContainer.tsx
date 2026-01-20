dc0938217c4a8c525cd73e2d0390e7b8d6bb8561
Michael Haschke <michael.haschke@eccenca.com>
2026-01-20 12:43:36 +0100

    Revert "add some default margin to each side"
    This reverts commit b99587f1fc889db3416a122b9a2f7c8e9b4178e0.

--- src/components/Typography/WhiteSpaceContainer.tsx
+++ src/components/Typography/WhiteSpaceContainer.tsx
@@ -4,7 +4,7 @@ import { CLASSPREFIX as eccgui } from "../../configuration/constants";
 
 import * as TypographyClassNames from "./classnames";
 
-type WhiteSpaceSizes = "none" | "tiny" | "small" | "regular" | "large" | "xlarge";
+type WhiteSpaceSizes = "tiny" | "small" | "regular" | "large" | "xlarge";
 
 export interface WhiteSpaceContainerProps extends React.HTMLAttributes<HTMLDivElement> {
     /**
@@ -52,10 +52,10 @@ export interface WhiteSpaceContainerProps extends React.HTMLAttributes<HTMLDivEl
 export const WhiteSpaceContainer = ({
     className,
     children,
-    marginTop = "regular",
-    marginRight = "regular",
-    marginBottom = "regular",
-    marginLeft = "regular",
+    marginTop,
+    marginRight,
+    marginBottom,
+    marginLeft,
     paddingTop,
     paddingRight,
     paddingBottom,
